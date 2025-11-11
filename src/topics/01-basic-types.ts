52356e8cd03570f45f6066679a9371b3a66883b7
Robin San Martin <robin.sanmartin.venegas@ciisa.cl>
2025-11-10 22:32:07 -0300

    added 01-basic-types.ts

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,23 @@
+
+const name = 'Robin';
+let hpPoints: number | string = 95;
+//let hpPoints: number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+
+hpPoints = 'FULL';
+hpPoints = 95;
+
+console.log(
+    name,
+    hpPoints,
+    isAlive
+);
+
+
+
+
+export {};
+
+
+
